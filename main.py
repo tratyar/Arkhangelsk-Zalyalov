80973f1dbfc2847cfe50c78a81aed711e6b4ce32
tratyar <sanyokzalyalov@mail.ru>
2024-11-28 16:52:03 +0300

    a

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
-def print_hi(name):
-    print(f'Hi, {name}')
+def main():
+    print('My first git program')
+    print('And I change it every day')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
